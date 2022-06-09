8df83b0101846c7aabd8925e9135b91d63ba821f
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-09 17:19:26 +0300

    commit tree 390f4a4086c1adaedd5e71c790a5261de76db1ff

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function window(customerName) {
+    return customerName()
+}
+
+/*function upperCaseCustomerName(){
+    return customerName.toUpperCase()
+*/
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+  }
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+  }
+
+const leastFavoriteCustomer = 'Otis'  
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Glenn'
+  }
